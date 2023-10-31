07db8449fafe55ec27264de5f55f8e01b2d99745
Pardeepkd2210 <149461879+Pardeepkd2210@users.noreply.github.com>
2023-10-31 11:46:13 +0530

    Add files via upload

---
+++ ApiClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CardsApi.Core.Client
+{
+    
+    public class ApiClient : IApiClient
+    {
+        private readonly HttpClient client;
+        public ApiClient()
+        {
+            client = new HttpClient();
+        }
+        public async Task<HttpResponseMessage> ExecuteAsync(HttpRequestMessage request)
+        {
+            ValidateInputs(request);
+            HttpResponseMessage response = null;
+            try
+            {
+                response = await client.SendAsync(request);
+                if (response == null)
+                {
+                    throw new OperationCanceledException($"No response from service at {request.RequestUri}, (response is null).");
+                }
+                else if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Something went wrong!! Please try again latter.");
+                }
+            }
+            catch (Exception ex)
+            {   
+                throw ex;
+            }
+            return response;
+        }
+
+        private void ValidateInputs(HttpRequestMessage request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentException("request must be non-null.");
+            }
+            if (request.RequestUri == null || request.Method == null)
+            {
+                throw new ArgumentException("request.RequestUri and request.Method must both be specified.");
+            }
+        }
+    }
+}
---
+++ IApiClient.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CardsApi.Core.Client
+{
+    public interface IApiClient
+    {
+        Task<HttpResponseMessage> ExecuteAsync(HttpRequestMessage request);
+    }
+}
